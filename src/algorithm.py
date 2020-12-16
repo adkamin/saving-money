0d056ad554ab590cfa5bf87e2e153189b631ee93
Thomas <thomaskolb98@gmail.com>
2020-12-16 17:15:14 +0100

    stuff

--- src/algorithm.py
+++ src/algorithm.py
@@ -5,6 +5,7 @@ costs = []
 savings = {}
 counter = 0
 
+debug = False
 
 def find_min_cost():
     global nr_dividers, costs, savings, counter
@@ -19,7 +20,7 @@ def find_min_cost():
     saved, used = saving_money(0, len(costs) - 1, 0)
     # print(f'saved: {saved}')
     # print(f'used: {used}', file=sys.stderr)
-    print(f'c={counter}, u={used}')
+    print(f'c={counter}, u={used}', file=sys.stderr)
     return total_cost - saved
 
 def read_input():
@@ -39,24 +40,26 @@ def saving_money(i, j, d):
     counter += 1
     # print(counter)
     # Memoization step:
-    # if (i,j) in savings:
-    #     print(f'i={i},j={j},savings={savings[(i,j)]}')
-    #     max_pair = (-2,0)
-    #     max_found = False
-    #     # print(len(savings[(i,j)]))
-    #     for (cost, dividers) in savings[(i,j)]:
-    #         if cost > max_pair[0] and dividers <= (nr_dividers - d):
-    #             max_pair = cost, dividers
-    #             # print("hiiiiiii")
-    #             # print(f'({i},{j}): {cost}; dividers={dividers}, nr_dividers={nr_dividers}, d={d}')
-    #             max_found = True
-    #     if max_found:
-    #         return max_pair
+    if (i,j) in savings:
+        if debug:
+            print(f'i={i},j={j},savings={savings[(i,j)]}', file=sys.stderr)
+        max_pair = (-2,0)
+        max_found = False
+        # print(len(savings[(i,j)]))
+        for (cost, dividers) in savings[(i,j)]:
+            if cost > max_pair[0] and dividers <= (nr_dividers - d):
+                max_pair = cost, dividers
+                # print("hiiiiiii")
+                # print(f'({i},{j}): {cost}; dividers={dividers}, nr_dividers={nr_dividers}, d={d}')
+                max_found = True
+        if max_found:
+            return max_pair
 
     # Base case 1
     if d >= nr_dividers:
         cost_sum = sum(costs[i:j + 1])
-        print(f'case 1, d = nr_dividers: {d} = {nr_dividers}, returning {cost_sum - round5(cost_sum)}')
+        if debug:
+            print(f'case 1, d = nr_dividers: {d} = {nr_dividers}, returning {cost_sum - round5(cost_sum)}', file=sys.stderr)
         if (i,j) in savings:
             savings[(i,j)].append((cost_sum - round5(cost_sum), 0))
         else:
@@ -65,7 +68,8 @@ def saving_money(i, j, d):
 
     # Base case 2
     if i == j:
-        print(f'case 2, i == j: {i} = {j}, returning {costs[i] - round5(costs[i])}')
+        if debug:
+            print(f'case 2, i == j: {i} = {j}, returning {costs[i] - round5(costs[i])}', file=sys.stderr)
         if (i,j) in savings:
             savings[(i,j)].append((costs[i] - round5(costs[i]), 0))
         else:
@@ -74,21 +78,22 @@ def saving_money(i, j, d):
 
     # Base case 3
     if i == j - 1:
-        print("case 3")
+        if debug:
+            print("case 3", file=sys.stderr)
         cost_sum = costs[i] + costs[j]
         with_divider = cost_sum - (round5(costs[i]) + round5(costs[j]))
         without_divider = cost_sum - round5(cost_sum)
         if with_divider > without_divider:
             if (i,j) in savings:
-                savings[(i,j)].append((without_divider, 1))
+                savings[(i,j)].append((with_divider, 1))
             else:
-                savings[(i,j)] = [(without_divider, 1)]
+                savings[(i,j)] = [(with_divider, 1)]
             return with_divider, 1
         else:
             if (i,j) in savings:
-                savings[(i,j)].append((with_divider, 0))
+                savings[(i,j)].append((without_divider, 0))
             else:
-                savings[(i,j)] = [(with_divider, 0)]
+                savings[(i,j)] = [(without_divider, 0)]
             return without_divider, 0
 
     # Recursive case
@@ -97,23 +102,29 @@ def saving_money(i, j, d):
         if k == j:
             cost_sum = sum(costs[i:j + 1])
             pair = (cost_sum - round5(cost_sum), 0)
-            print(f'k == j, {pair}')
+            if debug:
+                print(f'k == j, {pair}', file=sys.stderr)
         else:
-            print(f'k < j')
+            if debug:
+                print(f'k < j', file=sys.stderr)
             first = saving_money(i, k, d + 1)
-            print(f'first: {first}')
-            second = saving_money(k + 1, j, d + 1)
-            print(f'second: {second}')
+            if debug:
+                print(f'first: {first}', file=sys.stderr)
+            second = saving_money(k + 1, j, d + 1 + first[1])
+            if debug:
+                print(f'second: {second}', file=sys.stderr)
             pair = (first[0] + second[0], first[1] + second[1] + 1)
-            print(f'total: {pair}')
-        if pair[0] > max_pair[0] or (pair[0] == max_pair[0] and pair[1] > max_pair[1]):
+            if debug:
+                print(f'total: {pair}', file=sys.stderr)
+        if pair[0] > max_pair[0] or (pair[0] == max_pair[0] and pair[1] < max_pair[1]):
             max_pair = pair
     if (i,j) in savings:
         savings[(i,j)].append(max_pair)
     else:
         savings[(i,j)] = [max_pair]
-    print(f' max pair: {max_pair}')
-    return max_pair  # tuple of money saved and dividers used
+    if debug:
+        print(f' max pair: {max_pair}', file=sys.stderr)
+    return max_pair # tuple of money saved and dividers used
 
 def round5(x):
     return 5 * round(x / 5)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,30 @@
 from algorithm import find_min_cost
+import sys
+import time
+import operator as op
+from functools import reduce
 
 def main():
     while True:
         try:
+            start = time.process_time()
             print(find_min_cost())
+            end = time.process_time()
+            print(f'Time elapsed: {end - start} seconds', file=sys.stderr)
         except:
             break
 
+def a(products, dividers):
+    total = 0
+    for d in range(dividers+1):
+        total += ncr(products-1, d)
+    return total
+
+def ncr(n, r):
+    r = min(r, n-r)
+    numer = reduce(op.mul, range(n, n-r, -1), 1)
+    denom = reduce(op.mul, range(1, r+1), 1)
+    return numer // denom  # or / in Python 2
 
 if __name__ == '__main__':
     main()
